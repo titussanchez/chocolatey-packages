c37c4200db12a08c92f052188eaa3ab051081252
Titus Sanchez <titusjo@gmail.com>
2015-11-16 09:55:57 -0500

    Initial commit of norman-keyboard package
    Only tested on 64-bit version of Windows

---
+++ norman-keyboard/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'norman-keyboard'
+$installerType = 'msi'
+$url = 'https://normanlayout.info/files/Norman.zip'
+$silentArgs = '/q'
+$validExitCodes = @(0)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+
+if ($(Get-ProcessorBits 32)) {
+  $target = Join-Path $toolsDir "Norman/Norman_i386.msi"
+} else {
+  $target = Join-Path $toolsDir "Norman/Norman_amd64.msi"
+}
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$target" -validExitCodes $validExitCodes
---
+++ norman-keyboard/tools/chocolateyuninstall.ps1
@@ -0,0 +1,22 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'norman-keyboard'
+$installerType = 'msi'
+$silentArgs = '/q'
+$validExitCodes = @(0)
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ($(Get-ProcessorBits 32)) {
+  $file = Join-Path $toolsDir "Norman/Norman_i386.msi"
+} else {
+  $file = Join-Path $toolsDir "Norman/Norman_amd64.msi"
+}
+
+$env:chocolateyInstallArguments = "$file $silentArgs"
+$env:chocolateyInstallOverride = $TRUE
+
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes
